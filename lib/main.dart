ef76f8841cd446bbec6a90b42c9157bdfcef2539
Juan Carlos Zepeda <juan97zepda@gmail.com>
2020-05-25 21:49:40 -0600

    UserList

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,82 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(MyApp());
-}
+void main () => runApp(UserListApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class UserListApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('User List'),
+        ),
+        body: UserList(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+class User { //Se crea la clase User con sus atributos
+  String fullname, username, photoUrl;
+  User(this.fullname, this.username, this.photoUrl);
+  User.fromJson(Map<String, dynamic> json) //constructor que permite darle un MAP y establece los datos deseados a utilizar
+    : fullname = json['name']['first'] + ' ' + json['name']['last'],
+     username = json['login']['username'],
+     photoUrl = json['picture']['medium'];
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class UserList extends StatefulWidget {
+  @override
+  _UserListState createState() => _UserListState();
+}
 
-  void _incrementCounter() {
+class _UserListState extends State<UserList> {
+  bool loading;
+  List <User> users; //Una lista de tipo User para utilizar sus atributos
+
+  @override
+  void initState() { 
+    users = [];  //Se establecen los atributos fullname, username y photoUrl
+    loading = true;
+    _loadUsers();
+    super.initState();
+  }
+
+  void _loadUsers() async { //Traer datos de la API randomusers.me
+    final url = 'https://randomuser.me/api/?results=20'; //Se establece ls URL para hacer el metodo get
+    final response = await http.get(url); //Guarda en response el arreglo obtenido de datos
+    final json = jsonDecode(response.body); //se decodifica el String body del response en Json y se guarda en la variable
+    List <User> _users = [];
+    for (var jsonUser in json['results']) { //recorre todos los datos dentro del arreglo Results que devuelve la api y así recorre sus datos de los usuarios random
+     _users.add(User.fromJson(jsonUser)); //inserta en la lista _users los datos desde el constructor de la clase User. 
+    }
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      users = _users;
+      loading = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    if (loading) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return ListView.builder( //Crea un vista de lista
+      itemBuilder: (context, index) {
+        return ListTile( //Establece el contenido de cada row
+          title: Text(users[index].fullname), 
+          subtitle: Text(users[index].username),
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(users[index].photoUrl),
+          ),
+        );
+      },
+      itemCount: users.length, //establece la cantidad que existe de Usuarios, los cuenta y crea de acuerdo a cuantos existan en el arreglo con datos.
     );
   }
 }
